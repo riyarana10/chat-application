699fe39a695b5139efebd9a8df497032f2304a7f
riyarana10 <riyarana7865@gmail.com>
2025-02-15 21:30:51 +0530

    Added setup for frontend and backend for chat application

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+    res.send("Hello from signup route");
+};
+
+export const login = (req, res) => {
+    res.send("Hello from login route");
+};
+
+export const logout = (req, res) => {
+    res.send("Hello from logout route");
+};
