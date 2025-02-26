f8e7ff637474d666e814d20aca2ae60f7cc82cdd
riyarana10 <riyarana7865@gmail.com>
2025-02-26 16:52:01 +0530

    Added chat container in home page

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -10,6 +10,7 @@ export const useAuthStore = create((set, get) => ({
   isLoggingIn: false,
   isUpdatingProfile: false,
   isCheckingAuth: true,
+  onlineUsers: [],
 
   checkAuth: async () => {
     try {
